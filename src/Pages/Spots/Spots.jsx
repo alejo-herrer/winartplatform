dd6409774239954a50955053500bba487fd5e8a6
AlejoHerrer <alejandroherreralbengrin@gmail.com>
2026-05-11 20:22:02 -0500

    Winart Platform 1.0.5

--- src/Pages/Spots/Spots.jsx
+++ src/Pages/Spots/Spots.jsx
@@ -5,7 +5,9 @@ export function Spots(){
     return(
 
         <div className="wt-Spots">
-            
+            <div className="wt-Spots-container">
+                
+            </div>
         </div>
 
     )
