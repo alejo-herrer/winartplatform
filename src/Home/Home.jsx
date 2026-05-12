4e179c8c9ec8a9e3cf559b632c879962dfa7cfa2
AlejoHerrer <alejandroherreralbengrin@gmail.com>
2026-05-12 15:32:41 -0500

    Winart Platform 1.0.6

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 import { useScrollProgress } from '../Hook/useScroll';
-import { WButton } from '../components/WButton';
+import { WButton } from '../components/WButton'
 import { hoja1, hoja1_2, hoja2, hoja2_2, winartlogo, brillo, sol, videopresentacion} from '../assets'
 import './Home.css'
 
@@ -51,9 +51,9 @@ export function Home() {
                 </div>
                 <WButton>
                         Vive la experiencia WINART
-                        {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-                        </svg> */}
+                        </svg>
                 </WButton>
                 <p>18 - 20 DE FEBRERO DEL 2027 | <strong>LIMA, PERÚ</strong></p>
             </div>
--- src/Pages/Spots/Spots.jsx
+++ src/Pages/Spots/Spots.jsx
@@ -1,3 +1,4 @@
+import { WButton } from '../../components/WButton';
 import "./Spots.css"
 
 export function Spots(){
@@ -6,7 +7,22 @@ export function Spots(){
 
         <div className="wt-Spots">
             <div className="wt-Spots-container">
-                
+                <div className="wt-Spots-title">
+                    <span>⚠️</span>
+                    <span>🔥 CUPOS 🔥</span>
+                    <span>MUY LIMITADOS</span>
+                </div>
+                <p>Reserva tu lugar ahora antes que se agoten</p>
+            </div>
+            <br/>
+            <div>
+                <WButton>
+                        Vive la experiencia WINART
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
+                        </svg>
+                </WButton>
+                <p>18 - 20 DE FEBRERO DEL 2027 | <strong>LIMA, PERÚ</strong></p>
             </div>
         </div>
 
