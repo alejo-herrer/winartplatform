2e4408dd138e6173df55f6bc3d420b8cf7f8c183
AlejoHerrer <alejandroherreralbengrin@gmail.com>
2026-05-12 20:38:05 -0500

    Winart Platform 1.0.8.3

--- src/components/WTimeline.jsx
+++ src/components/WTimeline.jsx
@@ -1,38 +1,65 @@
+import { useIsMobile } from '../Hook/useIsMobile'
+import { useEffect, useState } from "react"
 import './Components.css'
 
 export function WTimeline({type = 1, txtPosition = 0, children, urlImg}){
 
-    return(
-       
-        <div className='wt-Timeline'>
-            <div className='wt-Timeline-txt'>
-                <div className='wt-Timeline-box'>
-                    {
-                        txtPosition === 0
-                        ? children
-                        :   <div>
-                                <img src={urlImg}></img>
-                            </div>
-                    }
-                </div>
-            </div>
-            <div className={`wt-Timeline${type}-line`}>
-                <div className={`wt-Timeline${type}-circle`}>
-                </div>
-            </div>
-            <div className='wt-Timeline-txt'>
-                <div className='wt-Timeline-box'>
-                    {
-                        txtPosition === 0
-                        ?    <div>
-                                <img src={urlImg}></img>
-                            </div>
-                        :   children
-                    }
-                </div>
-            </div>
-        </div>
+    const isMobile = useIsMobile()
+
+
+    if(!isMobile){
+        return(
         
-    )
+            <div className='wt-Timeline'>
+                <div className='wt-Timeline-txt'>
+                    <div className='wt-Timeline-box'>
+                        {
+                            txtPosition === 0
+                            ? children
+                            :   <div>
+                                    <img src={urlImg}></img>
+                                </div>
+                        }
+                    </div>
+                </div>
+                <div className={`wt-Timeline${type}-line`}>
+                    <div className={`wt-Timeline${type}-circle`}>
+                    </div>
+                </div>
+                <div className='wt-Timeline-txt'>
+                    <div className='wt-Timeline-box'>
+                        {
+                            txtPosition === 0
+                            ?    <div>
+                                    <img src={urlImg}></img>
+                                </div>
+                            :   children
+                        }
+                    </div>
+                </div>
+            </div>
+
+        )
+    } else {
+        return(
+        
+            <div className='wt-Timeline'>
+                <div className='wt-Timeline-txt'>
+                    <div className='wt-Timeline-box'>
+                        {children}
+                    </div>
+                </div>
+                <div className='wt-Timeline-txt'>
+                    <div className='wt-Timeline-box'>
+                        <div>
+                            <img src={urlImg}></img>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        )
+    }
 
 }
+
